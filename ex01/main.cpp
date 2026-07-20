ac5678852550d9449b973137b6c64a6d721fc440
Mika Schipper <mschippe@student.codam.nl>
2026-07-20 12:43:44 +0200

    make cat idea more recognizable in between all the ctor spam

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,29 +5,39 @@
 
 int main()
 {
-    const Animal* d = new Dog();
-    const Animal* c = new Cat();
-    delete d;//should not create a leak
-    delete c;
+	const Animal* d = new Dog();
+	const Animal* c = new Cat();
+	delete d;//should not create a leak
+	delete c;
 
-    Animal* animals[10];
-    for (int i = 0; i < 5; i++)
-        animals[i] = new Dog();
-    for (int i = 5; i < 10; i++)
-        animals[i] = new Cat();
+	// Create list of 10 animals
+	Animal* animals[10];
+	// Add 5 dogs to the list
+	for (int i = 0; i < 5; i++)
+		animals[i] = new Dog();
+	// Add 5 cats to the list
+	for (int i = 5; i < 10; i++)
+		animals[i] = new Cat();
 
-    // make a new cat
-    Cat* base = new Cat();
-    base->addIdea("Idea added on base cat", 0);
-    Cat* second = new Cat(*base);
-    // delete original cat
-    delete base;
-    // print idea in copied cat to prove it was deep copied
-    second->makeSound();
-    // get rid of second again
-    delete second;
+	for (int i = 0; i < 10; i++)
+	{
+		// Have each animal make a sound to show they are cats and dogs
+		animals[i]->makeSound();
+		// Then delete the animal as they will no longer be used
+		// This should not have leaks
+		delete animals[i];
+	}
 
-    for (int i = 0; i < 10; i++)
-        delete animals[i];
-    return 0;
+	// make a new cat
+	Cat* base = new Cat();
+	base->addIdea("BASE CAT'S GENIUS IDEA", 0);
+	Cat* second = new Cat(*base);
+	// delete original cat
+	delete base;
+	// print the first idea in the copied cat
+	// this should print out the idea that we added in the now-deleted base cat
+	std::cout << second->getIdea(0) << std::endl;
+	// get rid of second again
+	delete second;
+	return 0;
 }
